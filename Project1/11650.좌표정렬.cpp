17d8c9840c34dd1b35542fa5a3506bbbb02c826c
seoyoung-0 <kdy03013@gmail.com>
2020-02-01 16:01:14 +0900

    200131-study

---
+++ Project1/11650.좌표정렬.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+	vector<pair<int, int>> v;
+
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		scanf("%d %d", &x, &y);
+
+		v.push_back(make_pair(x, y));
+
+	}
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", v[i].first, v[i].second);
+	}
+
+	return 0;
+
+}
